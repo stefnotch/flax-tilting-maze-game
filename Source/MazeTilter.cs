62864c699d430d36906fc7aa281c308780f98582
Stefnotch <brandmairstefan@gmail.com>
2018-09-30 11:11:26 +0200

    Renamed something

--- Source/MazeTilter.cs
+++ Source/MazeTilter.cs
@@ -10,9 +10,9 @@ namespace AMaze
 		public RigidBody GeneratedMaze;
 		public float Damping = 0.1f;
 		public float Speed = 1f;
-		private float _pitch;
-		private float _yaw;
-		private float _roll;
+		private float _pitchSpeed;
+		private float _yawSpeed;
+		private float _rollSpeed;
 
 		private void Start()
 		{
@@ -24,30 +24,32 @@ namespace AMaze
 			// Here you can add code that needs to be called every frame
 			if (Input.GetKey(Keys.ArrowUp))
 			{
-				_roll -= Speed;
+				_rollSpeed -= Speed;
 			}
 			if (Input.GetKey(Keys.ArrowDown))
 			{
-				_roll += Speed;
+				_rollSpeed += Speed;
 			}
 			if (Input.GetKey(Keys.ArrowLeft))
 			{
-				_pitch += Speed;
+				_pitchSpeed += Speed;
 			}
 			if (Input.GetKey(Keys.ArrowRight))
 			{
-				_pitch -= Speed;
+				_pitchSpeed -= Speed;
 			}
 
 			GeneratedMaze.AngularDamping = Damping;
 		}
 
+		private Quaternion _orientation;
+
 		private void FixedUpdate()
 		{
-			GeneratedMaze.AngularVelocity += new Vector3(_pitch, _yaw, _roll) * Time.DeltaTime;
-			_pitch = _pitch - _pitch * Damping * Time.DeltaTime;
-			_yaw = _yaw - _yaw * Damping * Time.DeltaTime;
-			_roll = _roll - _roll * Damping * Time.DeltaTime;
+			GeneratedMaze.AngularVelocity += new Vector3(_pitchSpeed, _yawSpeed, _rollSpeed) * Time.DeltaTime;
+			_pitchSpeed = _pitchSpeed - _pitchSpeed * Damping * Time.DeltaTime;
+			_yawSpeed = _yawSpeed - _yawSpeed * Damping * Time.DeltaTime;
+			_rollSpeed = _rollSpeed - _rollSpeed * Damping * Time.DeltaTime;
 
 			Maze.LocalOrientation = GeneratedMaze.LocalOrientation;
 		}
